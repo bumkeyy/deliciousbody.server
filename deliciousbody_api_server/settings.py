181c4bef7a931e49060e375fe64bdf58ffc3ab6d
kibeom <kgbum2222@gmail.com>
2018-08-21 22:25:27 +0900

    [DB] PostgreSQL 연동 & requirement.txt 생성

--- deliciousbody_api_server/settings.py
+++ deliciousbody_api_server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'deliciousbody_api_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'delicious_database',
+        'USER': 'sample_user',
+        'PASSWORD': 'sample_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
