e321c721dd9000bc43f09153c4405e61ebb55c42
kibeom <kgbum2222@gmail.com>
2018-09-27 15:10:53 +0900

    [DB] gcloud db로 변경

--- deliciousbody_api_server/settings.py
+++ deliciousbody_api_server/settings.py
@@ -102,6 +102,7 @@ WSGI_APPLICATION = 'deliciousbody_api_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -112,7 +113,17 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'delicious_database',
+        'USER': 'bumkey',
+        'PASSWORD': 'elfqk_1290',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
