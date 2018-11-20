a82d2ba85521365ef31533b41a5df3be2d582008
kibeom <kgbum2222@gmail.com>
2018-11-20 14:26:17 +0900

    [Video] modify admin

--- video/admin.py
+++ video/admin.py
@@ -4,6 +4,8 @@ from .models import Video
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('video_id', 'video_name', 'level', 'main_part', 'sub_part')
+    list_display_links = ['video_name']
+    search_fields = ['video_name']
     ordering = ('video_id',)  # The negative sign indicate descendent order
 
 
