828571cee0f314cd74311ee3eff8c86bdad8c0ab
kibeom <kgbum2222@gmail.com>
2018-11-24 19:43:14 +0900

    change static

--- config/settings/common_aws.py
+++ config/settings/common_aws.py
@@ -110,10 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
